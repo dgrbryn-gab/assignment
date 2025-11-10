d284bdf4ff9dbe6790ac93419a79f284dea9588b
dgrbryn-gab <dgrbryn@gmail.com>
2025-11-11 02:19:52 +0800

    Add in-memory authentication configuration

--- security_activity/src/Controller/DashboardController.php
+++ security_activity/src/Controller/DashboardController.php
@@ -11,6 +11,16 @@ final class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        return new Response("Welcome to the Dashboard! (Protected Area)");
+        $user = $this->getUser();
+
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            $message = 'You are logged in as Admin. You have full access.';
+        } else {
+            $message = 'You are logged in as Regular User. Limited access granted.';
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'role_message' => $message,
+        ]);
     }
 }
